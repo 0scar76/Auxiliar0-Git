86b3d8b091fba700badf6357f8529bb9e82a874c
Oscar <oscarconejerosb@gmail.com>
2026-03-18 19:24:55 -0300

    Afinal

--- usuario.py
+++ usuario.py
@@ -9,13 +9,7 @@ class Usuario:
         self.tareas.append(tarea)
 
     def listarTareas(self):
-<<<<<<< HEAD
-   for tarea in self.tareas:
-       if tarea.estaLista():
-           print(f"La tarea {tarea.obtenerNombre()} está lista")
-           print(f"La tarea {tarea.obtenerNombre()} no está lista")
-=======
-    for tarea in self.tareas:
-        if tarea.estaLista():
-            print(f"[X] {tarea.obtenerNombre()}" )
->>>>>>> b1d67f6f8802f3b7b2e8395c373d3e74d9b0203f
+        for tarea in self.tareas:
+            if tarea.estaLista():
+                print(f"La tarea {tarea.obtenerNombre()} está lista")
+                print(f"La tarea {tarea.obtenerNombre()} no está lista")
